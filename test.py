aa6a687eba39a20099c544957c7431a8f7ac26a3
Diola2604 <104823618+Diola2604@users.noreply.github.com>
2023-11-09 11:57:31 +0100

    Update test.py

--- test.py
+++ test.py
@@ -11,9 +11,38 @@ def test_creation_grille():
                               [0, 0, 0, 0, 0, 0, 0, 0, 0],
                               [0, 0, 0, 0, 0, 0, 0, 0, 0],
                               [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                              [0, 0, 0, 0, 0, 0, 0, 0, 0] ]
-
-def test_validation_sudoku():
+                              [0, 0, 0, 0, 0, 0, 0, 0, 0]]
+def test_verifie_liste():
+    sudoku = grille.Grille(9)
+    sudoku_a = [
+        [5, 3, 4, 6, 7, 8, 9, 1, 2],
+        [6, 7, 2, 1, 9, 5, 3, 4, 8],
+        [1, 9, 8, 3, 4, 2, 5, 6, 7],
+        [8, 5, 9, 7, 6, 1, 4, 2, 3],
+        [4, 2, 6, 8, 5, 3, 7, 9, 1],
+        [7, 1, 3, 9, 2, 4, 8, 5, 6],
+        [9, 6, 1, 5, 3, 7, 2, 8, 4],
+        [2, 8, 7, 4, 1, 9, 6, 3, 5],
+        [3, 4, 5, 2, 8, 6, 1, 7, 9]]
+    assert sudoku.verifie_liste(sudoku_a[0])==True
+    
+def test_indices_region():
+    sudoku = grille.Grille(9)
+    sudoku_b = [
+        [5, 3, 4, 6, 7, 8, 9, 1, 2],
+        [6, 7, 2, 1, 9, 5, 3, 4, 8],
+        [1, 9, 8, 3, 4, 2, 5, 6, 7],
+        [8, 5, 9, 7, 6, 1, 4, 2, 3],
+        [4, 2, 6, 8, 5, 3, 7, 9, 1],
+        [7, 1, 3, 9, 2, 4, 8, 5, 6],
+        [9, 6, 1, 5, 3, 7, 2, 8, 4],
+        [2, 8, 7, 4, 1, 9, 6, 3, 5],
+        [3, 4, 5, 2, 8, 6, 1, 7, 9]]
+    assert sudoku.indices_region(0,0)==[(0,1),(0,2),(1,0),(1,1),(1,2),(2,0),(2,1),(2,2)]
+    
+    
+    
+def test_est_sudoku():
     sudoku_valide = [
         [5, 3, 4, 6, 7, 8, 9, 1, 2],
         [6, 7, 2, 1, 9, 5, 3, 4, 8],
@@ -39,34 +68,7 @@ def test_validation_sudoku():
     sudoku = grille.Grille(9)
     assert sudoku.est_sudoku(sudoku_valide) == True
     assert sudoku.est_sudoku(sudoku_invalide)==False 
-        
-def test_completer_sudoku():
-    sudoku = [
-        [5, 3, 0, 0, 7, 0, 0, 0, 0],
-        [6, 0, 0, 1, 9, 5, 0, 0, 0],
-        [0, 9, 8, 0, 0, 0, 0, 6, 0],
-        [8, 0, 0, 0, 6, 0, 0, 0, 3],
-        [4, 0, 0, 8, 0, 3, 0, 0, 1],
-        [7, 0, 0, 0, 2, 0, 0, 0, 6],
-        [0, 6, 0, 0, 0, 0, 2, 8, 0],
-        [0, 0, 0, 4, 1, 9, 0, 0, 5],
-        [0, 0, 0, 0, 8, 0, 0, 7, 9]]
-
-    assert sudoku.completer_sudoku(sudoku)==[[5, 3, 4, 6, 7, 8, 9, 1, 2],
-                                             [6, 7, 2, 1, 9, 5, 3, 4, 8],
-                                             [1, 9, 8, 3, 4, 2, 5, 6, 7],
-                                             [8, 5, 9, 7, 6, 1, 4, 2, 3],
-                                             [4, 2, 6, 8, 5, 3, 7, 9, 1],
-                                             [7, 1, 3, 9, 2, 4, 8, 5, 6],
-                                             [9, 6, 1, 5, 3, 7, 2, 8, 4],
-                                             [2, 8, 7, 4, 1, 9, 6, 3, 5],
-                                             [3, 4, 5, 2, 8, 6, 1, 7, 9]]
-
-def test_creer_sudoku():
-    sudoku = grille.Grille(9)
-    sudoku_genere = sudoku.creer_sudoku()
-    print("Sudoku généré",sudoku_genere)
-
+    
 def test_tableau_case_vides():
     sudoku = grille.Grille(9)
     tableau = [
@@ -82,8 +84,6 @@ def test_tableau_case_vides():
     
     cases_vides = sudoku.tableau_case_vides(tableau)
     assert cases_vides == [(0, 2), (0, 3), (0, 5), (0, 6), (0, 7), (0, 8), (1, 1), (1, 2), (1, 6), (1, 7), (1, 8), (2, 0), (2, 3), (2, 4), (2, 5), (2, 6), (2, 8), (3, 1), (3, 2), (3, 3), (3, 5), (3, 6), (3, 7), (4, 1), (4, 2), (4, 4), (4, 6), (4, 7), (5, 1), (5, 2), (5, 3), (5, 5), (5, 6), (5, 7), (6, 0), (6, 2), (6, 3), (6, 4), (6, 5), (6, 8), (7, 0), (7, 1), (7, 2), (7, 6), (7, 7), (8, 0), (8, 1), (8, 2), (8, 3), (8, 5), (8, 6)]
-
-
 
 def test_choix_possibles():
     sudoku = grille.Grille(9)
@@ -103,12 +103,76 @@ def test_choix_possibles():
     valeurs_possibles = sudoku.choix_possibles(tableau, x, y)
     assert valeurs_possibles == [2, 6]
 
+def test_selectionner_case():
+    pass
+    
+    
+def test_ajouter_valeur():
+    sudoku = grille.Grille(9)
+    sudoku_d = [
+        [5, 3, 4, 6, 7, 8, 9, 1, 2],
+        [6, 7, 2, 1, 9, 5, 3, 4, 8],
+        [1, 9, 8, 3, 4, 2, 5, 6, 7],
+        [8, 5, 9, 7, 6, 1, 4, 2, 3],
+        [4, 2, 6, 8, 5, 3, 7, 9, 1],
+        [7, 1, 3, 9, 2, 4, 8, 5, 6],
+        [9, 6, 1, 5, 3, 7, 2, 8, 4],
+        [2, 8, 7, 4, 1, 9, 6, 3, 5],
+        [3, 4, 5, 2, 8, 6, 0, 7, 9]]
+    sudoku.ajouter_valeur(sudoku_d, 1, 8, 6)
+    assert sudoku_d==[[5, 3, 4, 6, 7, 8, 9, 1, 2],
+                      [6, 7, 2, 1, 9, 5, 3, 4, 8],
+                      [1, 9, 8, 3, 4, 2, 5, 6, 7],
+                      [8, 5, 9, 7, 6, 1, 4, 2, 3],
+                      [4, 2, 6, 8, 5, 3, 7, 9, 1],
+                      [7, 1, 3, 9, 2, 4, 8, 5, 6],
+                      [9, 6, 1, 5, 3, 7, 2, 8, 4],
+                      [2, 8, 7, 4, 1, 9, 6, 3, 5],
+                      [3, 4, 5, 2, 8, 6, 1, 7, 9]]
+
+
+
+def test_completer_sudoku():
+    s = grille.Grille(9)
+    sudoku = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]]
+    assert s.completer_sudoku(sudoku)==(True,[[5, 3, 4, 6, 7, 8, 9, 1, 2],
+                                        [6, 7, 2, 1, 9, 5, 3, 4, 8],
+                                        [1, 9, 8, 3, 4, 2, 5, 6, 7],
+                                        [8, 5, 9, 7, 6, 1, 4, 2, 3],
+                                        [4, 2, 6, 8, 5, 3, 7, 9, 1],
+                                        [7, 1, 3, 9, 2, 4, 8, 5, 6],
+                                        [9, 6, 1, 5, 3, 7, 2, 8, 4],
+                                        [2, 8, 7, 4, 1, 9, 6, 3, 5],
+                                        [3, 4, 5, 2, 8, 6, 1, 7, 9]])
+
+def test_creer_sudoku():
+    sudoku = grille.Grille(9)
+    sudoku_genere = sudoku.creer_sudoku()
+    print("Sudoku généré",sudoku_genere)
+
+
+
+
 
 
 if __name__ == "__main__":
     test_creation_grille()
-    test_validation_sudoku()
-    test_completer_sudoku()
-    test_creer_sudoku()
+    test_verifie_liste()
+    test_indices_region()
+    test_est_sudoku()
     test_tableau_case_vides()
     test_choix_possibles()
+    test_selectionner_case()
+    test_ajouter_valeur()
+    test_completer_sudoku()
+    #test_creer_sudoku()
+
