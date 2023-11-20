441ac3fa35d58a98045dd264efa826158b121bd8
243kher <109959394+243kher@users.noreply.github.com>
2023-11-20 21:20:44 +0100

    Update Optimal.py

--- Optimal.py
+++ Optimal.py
@@ -9,52 +9,90 @@ from  Grille import Grille
 import time 
 
 class Optimal:
-    def __init__(self, type_sudoku):
-        self.type_sudoku = type_sudoku
-        
-    def chiffres_min(self):
-        grille = Grille(self.type_sudoku)
-        continuer = True
-        minimum = self.type_sudoku**2
-        while continuer :
-            tableau= grille.creer_sudoku()
-            n = len(grille.tableau_case_vides(tableau))
-            if n < minimum:
-                minimum = n 
-            arreter = input("Arreter")
-            if arreter == 'oui':
-                continuer = False
-                
-        return f"Le nombre minimal moyen de valeurs d'un sudoku nouveau est de {minimum}"
-            
-    def temps_generer_sudoku(self):
-        grille = Grille(self.type_sudoku)
-        continuer = True
-        minimum = time.time()
-        while continuer :
-            temps_a = time.time()
-            tableau= grille.creer_sudoku()
-            temps_b = time.time()
-            n = temps_b -  temps_a
-            if n < minimum:
-                minimum = n 
-            arreter = input("Arreter")
-            if arreter == 'oui':
-                continuer = False
-            print(minimum)
-        return f"Le temps minimal moyen pour generer un sudoku nouveau est de {minimum}secondes"
+    def __init__(self, nb_sudokus, taille_grille):
+        self.nb_sudokus = nb_sudokus
+        self.taille_grille = taille_grille
+        self.sudokus = []
+
+    def generer_sudoku(self):
+        """
+        Génère un sudoku incomplet et compte le nombre de valeurs.
+        """
+        grille = Grille(self.taille_grille)
+        sudoku = grille.creer_sudoku()
+
+        # Compte le nombre de valeurs dans un sudoku
+        nb_valeurs = sum(rangee.count(0) for rangee in sudoku)
+
+        return sudoku, nb_valeurs
+
+    def generer_et_sauvegarder_sudokus(self, nom_fichier):
+        """
+        Génère et enregistre les sudokus dans un fichier trié par nombre de valeurs.
+        """
+        n=0
+        for _ in range(self.nb_sudokus):
+            n=n+1
+            print(n)
+            sudoku, nb_valeurs = self.generer_sudoku()
+            self.sudokus.append((sudoku, nb_valeurs))
+
+        # Trie les sudokus par nombre de valeurs
+        self.sudokus.sort(key=lambda x: x[1])
+
+        # Enregistre les sudokus dans le fichier
+        with open(nom_fichier, 'w') as fichier:
+            for sudoku, nb_valeurs in self.sudokus:
+                fichier.write(f"Le nombre de valeurs est:{nb_valeurs}\n")
+                for rangee in sudoku:
+                    fichier.write(" ".join(map(str, rangee)) + "\n")
+                fichier.write("\n")
 
 
-    def temps_completer_sudoku(self):
-        grille = Grille(self.type_sudoku)
-        continuer = True
-        temps_a = time.time()
-        tableau= grille.creer_sudoku_complet()
-        temps_b = time.time()
-        n = temps_b -  temps_a
-            
-        return f"Le temps pour completer un sudoku vide est de {n} secondes"
+    def completer_et_mesurer_temps(self, sudokus, nom_fichier):
+        """
+        Methode qui va effectuer les sudokus et mesurez le temps nécessaire dans chaque cas en millisecondes.
+        Et enregistrer les résultats dans un fichier classé.
+        """
+        resultats = []
 
-n= Optimal(9)
-print(n.temps_completer_sudoku())
+        for sudoku, nb_valeurs in sudokus:
+            debut_temps = time.time()
+
+            # Essaye de compléter le sudoku et mesure le temps.
+            reussite, sudoku_termine = Grille(self.taille_grille).completer_sudoku(sudoku)
+
+            fin_temps = time.time()
+            temps_ecoule = (fin_temps - debut_temps) * 1000  # Convertir en millisecondes
+            print(temps_ecoule)
+            if reussite:
+                resultats.append((sudoku_termine, temps_ecoule, nb_valeurs))
+
+
+        # Enregistre les résultats dans le fichier
+        with open(nom_fichier, 'w') as fichier:
+            for sudoku_termine, temps_ecoule, nb_valeurs in resultats:
+                fichier.write(f"Nb valeurs: {nb_valeurs}, Temps ecoule: {temps_ecoule:.2f} milliseconds\n")
+                for rangee in sudoku_termine:
+                    fichier.write(" ".join(map(str, rangee)) + "\n")
+                fichier.write("\n")
+
+if __name__ == "__main__":
+    optimal = Optimal(nb_sudokus=30, taille_grille=9)
+    optimal.generer_et_sauvegarder_sudokus("sudokus.txt")
+
+    # Lee los sudokus generados
+    with open("sudokus2.txt", 'r') as fichier:
+        sudokus = []
+        lignes = fichier.readlines()
+        i = 0
+        while i < len(lignes):
+            nb_valeurs = int(lignes[i].split(":")[1].strip())
+            sudoku = [list(map(int, lignes[j].split())) for j in range(i + 1, i + 10)]
+            sudokus.append((sudoku, nb_valeurs))
+            i += 11
+
+
+
+    optimal.completer_et_mesurer_temps(sudokus, "resultats2.txt")
 
