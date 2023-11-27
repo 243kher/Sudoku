002ad61289f0c5414ef7894a79bb683745a2c31a
243kher <109959394+243kher@users.noreply.github.com>
2023-11-27 17:25:23 +0100

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -19,6 +19,10 @@ class SudokuGUI:
         self.create_widgets()
 
     def create_widgets(self):
+        """
+        Fonction qui genere la premiere fenetre 
+        pour nous donner le choix entre un sudoku 4*4 ou 9*9.
+        """
         label = tk.Label(self.interface, text="Sélectionnez la taille du Sudoku :")
         label.pack(pady=10)
 
@@ -29,6 +33,17 @@ class SudokuGUI:
         bouton_9x9.pack(pady=5)
 
     def demarrer_jeu(self, dimension):
+        """
+        Fonction qui en dependant de la dimension va ouvrir un jeu 4x4 ou 9x9.
+        la fenetre va contenir 3 boutons :
+            Verifier : qui va verifier si la reponse de l'utilisateur est correcte
+            Resouder :  qui va donner la reponse 
+            Nouveau jeu : Qui va generer un nouveau jeu
+
+        Args:
+            dimension (int): Dimension du jeu 9 ou 4.
+
+        """
         self.dimension.set(dimension)
         if self.fenetre:
             self.fenetre.destroy()
@@ -48,13 +63,20 @@ class SudokuGUI:
         verifier_bouton = tk.Button(self.fenetre, text="Vérifier la solution", command=lambda: self.verifier_solution(sudoku_tableau))
         verifier_bouton.grid(row=self.dimension.get(), column=0, columnspan=self.dimension.get(), pady=10)
 
-        resoudre_bouton = tk.Button(self.fenetre, text="Résoudre", command=lambda: self.solve_puzzle(sudoku_tableau))
+        resoudre_bouton = tk.Button(self.fenetre, text="Résoudre", command=lambda: self.resoudre_sudoku(sudoku_tableau))
         resoudre_bouton.grid(row=self.dimension.get() + 1, column=0, columnspan=self.dimension.get(), pady=10)
 
         nouveau_jeu_bouton = tk.Button(self.fenetre, text=f"Nouveau jeu {dimension}x{dimension}", command=lambda: self.demarrer_jeu(dimension))
         nouveau_jeu_bouton.grid(row=self.dimension.get() + 2, column=0, columnspan=self.dimension.get(), pady=10)
 
     def verifier_solution(self, tableau):
+        """
+        Fonction qui  va verifier si la reponse de l'utilisateur est correcte
+
+        Args:
+            tableau (list): Tableau qui contient le sudoku que le joueur a resolu 
+
+        """
         tableau_final = [int(entree.get()) for entree in self.fenetre.winfo_children() if isinstance(entree, tk.Entry)]
         solution_utilisateur = [tableau_final[i:i + self.dimension.get()] for i in range(0, len(tableau_final), self.dimension.get())]
     
@@ -67,11 +89,17 @@ class SudokuGUI:
         else:
             messagebox.showerror("Résultat", "Il y a quelque chose qui ne va pas. Réessayez !")
 
-    def solve_puzzle(self, tableau):
+    def resoudre_sudoku(self, tableau):
+        """
+        Fonction qui va donner la reponse 
+        Args:
+            tableau (list): Le sudoku de base 
+
+        """
         tableau_resolu = self.sudoku_jeu.completer_sudoku(tableau)[1]
 
-        for i, row in enumerate(tableau_resolu):
-            for j, valeur in enumerate(row):
+        for i, rangee in enumerate(tableau_resolu):
+            for j, valeur in enumerate(rangee):
                 entree = tk.Entry(self.fenetre, width=4, font=('Arial', 16), justify='center')
                 entree.grid(row=i, column=j)
                 entree.insert(0, str(valeur))
